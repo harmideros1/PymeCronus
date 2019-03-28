f2b815a427704c3fd5f1f528c2bc2bab3f171285
Harold Mideros <harmideros@gmail.com>
2019-03-27 22:22:18 -0500

    Configuración de FireBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  enviromentConfig: {
+    apiKey: "AIzaSyCdMTp-2UmJZDPaBfb8P1lBUWkrZ7kqfSk",
+    authDomain: "pymecronus.firebaseapp.com",
+    databaseURL: "https://pymecronus.firebaseio.com",
+    projectId: "pymecronus",
+    storageBucket: "pymecronus.appspot.com",
+    messagingSenderId: "1067924411260"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  enviromentConfig: {
+    apiKey: "AIzaSyCdMTp-2UmJZDPaBfb8P1lBUWkrZ7kqfSk",
+    authDomain: "pymecronus.firebaseapp.com",
+    databaseURL: "https://pymecronus.firebaseio.com",
+    projectId: "pymecronus",
+    storageBucket: "pymecronus.appspot.com",
+    messagingSenderId: "1067924411260"
+  }
 };
 
 /*
